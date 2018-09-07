a17c4f7c70c0ea44ff681531952b326fc61f10c3
Eddie Lau <eddie@touchutility.com>
2018-09-07 09:54:56 +0800

    fix typo.

--- rcuv.sh
+++ rcuv.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 node ./index.js -i ../rcuv-usfm/res/RCUV_USFM/ -e -l zh-Hant
-./output/epub/output.epub ./rcuv.epub --chapter "/" --page-breaks-before "//h:div[@class=\"page-break\"]"
+ebook-convert ./output/epub/output.epub ./rcuv.epub --chapter "/" --page-breaks-before "//h:div[@class=\"page-break\"]"
 kindlegen ./rcuv.epub
